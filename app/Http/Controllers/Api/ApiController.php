7b0c9c7fcfe779540e00f4a78f19c1e5db727d34
dukailong <dukailong@localhost.localdomain>
2019-03-13 04:01:40 +0000

    ppp

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ApiController extends Controller
+{
+    //
+    public function api()
+    {
+        echo 123123;
+    }
+}
